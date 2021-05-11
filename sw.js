1712f126a0bd31c27976f9e5cb829e499958257f
oylx <2236212702@qq.com>
2021-05-11 17:56:01 +0800

    Site updated: 2021-05-11 17:56:01

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","4789806aab28e8a61222d9a7e3603158"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
